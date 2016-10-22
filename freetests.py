87cfeb442f6b5dbf9ebdb94eb44fad01982df176
tbrockman <tbrockma@ualberta.ca>
2016-10-22 14:59:58 -0600

    tests passing

--- freetests.py
+++ freetests.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2013 Abram Hindle
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,7 +35,7 @@ class ServerTestCase(unittest.TestCase):
 
     def tearDown(self):
         '''nothing'''
-        
+
     def testNothing(self):
         '''nothing'''
 
@@ -64,7 +64,7 @@ class ServerTestCase(unittest.TestCase):
         self.assertTrue(r.status_code == 200, "Code not 200!")
         self.assertTrue(json.loads(r.data) == d, "D != r.data")
 
-        
+
     def populateWorld(self):
         self.world = dict()
         for i in range(1,20):
@@ -92,9 +92,9 @@ class ServerTestCase(unittest.TestCase):
             self.assertTrue(self.world[key]  == newworld[key], "Key %s" % key)
 
 
-        
-        
-        
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
